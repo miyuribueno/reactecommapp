75453598594822c0d8d23ec57cdb6ed2ad28533f
unknown <buenomiyuri@gmail.com>
2020-08-03 11:21:34 +0800

    Updates

---
+++ precache-manifest.3c2af7772d562b1ee621a6e5563e3c80.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92eff5180ffa50a39f3899eb8ae288e6",
+    "url": "/reactecommapp/index.html"
+  },
+  {
+    "revision": "a41462e91d9b42684d25",
+    "url": "/reactecommapp/static/css/main.9c8f29f0.chunk.css"
+  },
+  {
+    "revision": "1da6a9e9d6f1ca85399f",
+    "url": "/reactecommapp/static/js/2.6600b1cc.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/reactecommapp/static/js/2.6600b1cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a41462e91d9b42684d25",
+    "url": "/reactecommapp/static/js/main.e23f54a6.chunk.js"
+  },
+  {
+    "revision": "d5bf9ad792a70a7d00c0",
+    "url": "/reactecommapp/static/js/runtime-main.85a13bad.js"
+  },
+  {
+    "revision": "3d5da99e37e0973e8317c1a2570cc65c",
+    "url": "/reactecommapp/static/media/cart.3d5da99e.png"
+  },
+  {
+    "revision": "77c9b3ccd8e21b5431ec7a959b20be57",
+    "url": "/reactecommapp/static/media/logo.77c9b3cc.png"
+  }
+]);
